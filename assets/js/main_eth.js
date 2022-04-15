23442f0fb0540705add279a44da33de7ed85fa6c
arthurdefi <99156028+arthurdefi@users.noreply.github.com>
2022-04-15 18:29:16 +0800

    Update main_eth.js

--- assets/js/main_eth.js
+++ assets/js/main_eth.js
@@ -23,7 +23,7 @@ var withdrawCooldown=0;
 
 var contract;
 
-const minerAddress = '0xF65a689dB35fa88361DeF09F4eE309211e9E418e'; //testnet contract
+const minerAddress = '0x6666c0ae549e9869730d8997e35d0d55d419a5f0'; //testnet contract
 const tokenAddress = '0x78867BbEeF44f2326bF8DDd1941a4439382EF2A7'; //testnet BUSD
 //const minerAddress = ''; //mainnet contract   
 //const tokenAddress = '0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56'; //mainnet BUSD
