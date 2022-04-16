5557e07383784f3d4211dbe31e05fd3202db1869
arthurdefi <99156028+arthurdefi@users.noreply.github.com>
2022-04-16 14:43:29 +0800

    Update main_eth.js

--- assets/js/main_eth.js
+++ assets/js/main_eth.js
@@ -555,10 +555,10 @@ function setStartTimer() {
         // Find the distance between now and the count down date
         var distance = endDate - currTime;
         // Time calculations for days, hours, minutes and seconds
-        var days = Math.floor(timeleft / (1000 * 60 * 60 * 24));
-	var hours = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-	var minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
-	var seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+	var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+	var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+	var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 	
 	if (days < 10) { days = '0' + days; }
         if (hours < 10) { hours = '0' + hours; }
